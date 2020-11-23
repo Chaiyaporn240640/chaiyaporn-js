079a53791492eeca0466c1aa0e1666bc387a75ac
Chaiyaporn240640 <74871667+Chaiyaporn240640@users.noreply.github.com>
2020-11-23 10:23:02 +0700

    test arrow function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-let nickname = "your nickname";
-console.log(nickname);
+//function add(a, b) {
+//return a + b;
+//}
+
+const add = (a, b) => a + b; //{
+
+//return a + b;
+//}
+// const add = (a, b) => {
+//   return a + b;
+// }
+
+// const add = (a, b) => a + b
+
+console.log(add(1, 2));
